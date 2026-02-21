ef7af84629e0063d40c1dd22dce43936197b7003
sanaa <Sanaakumar07@gmail.com>
2026-02-21 13:29:57 +0530

    c

--- day15/main.c
+++ day15/main.c
@@ -1,15 +1,29 @@
-//
-//  main.c
-//  day15
-//
-//  Created by Sanaa Kumar on 21/02/26.
-//
-
-#include <stdlib.h>
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return EXIT_SUCCESS;
+int main(void)
+{
+    int n, i, j;
+    int a[100][100];
+    int sum = 0;
+
+    printf("Enter order of square matrix: ");
+    scanf("%d", &n);
+
+    printf("Enter elements of matrix:\n");
+    for(i = 0; i < n; i++)
+    {
+        for(j = 0; j < n; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    for(i = 0; i < n; i++)
+    {
+        sum += a[i][i];
+    }
+
+    printf("Sum of primary diagonal elements = %d\n", sum);
+
+    return 0;
 }
